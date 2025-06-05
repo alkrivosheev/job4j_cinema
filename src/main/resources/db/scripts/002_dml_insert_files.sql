347615c321f29d679d8fcb27789bf8a65b43927e
alkrivosheev <alkrivosheev@gmail.com>
2025-06-05 11:31:35 +0300

    add database & files

---
+++ src/main/resources/db/scripts/002_dml_insert_files.sql
@@ -0,0 +1,9 @@
+INSERT INTO files (name, path) VALUES
+('avatar.jpg', 'files/avatar.jpg'),
+('inception.jpg', 'files/inception.jpg'),
+('interstellar.jpg', 'files/interstellar.jpg'),
+('matrix.jpg', 'files/matrix.jpg'),
+('pulp_fiction.jpg', 'files/pulp_fiction.jpg'),
+('shawshank.jpg', 'files/shawshank.jpg'),
+('godfather.jpg', 'files/godfather.jpg'),
+('dark_knight.jpg', 'files/dark_knight.jpg');
---
+++ src/main/resources/db/scripts/005_dml_insert_halls.sql
@@ -0,0 +1,5 @@
+INSERT INTO halls (name, row_count, place_count, description) VALUES
+('Red Hall', 10, 20, 'Главный зал со звуковой системой Dolby Atmos'),
+('Blue Hall', 8, 15, 'VIP-зал с местами премиум-класса'),
+('Green Hall', 12, 25, 'Стандартный зал с 3D-проекцией'),
+('Gold Hall', 6, 12, 'Роскошный зал с диванами-сиденьями');
